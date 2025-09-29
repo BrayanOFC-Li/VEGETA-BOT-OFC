42f911c6d6e0afc4772629974b774838dab0e46c
BrayanOFC <martinezalexis84280@gmail.com>
2025-09-28 17:24:27 -0700

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn }) => {
   try {
     const start = Date.now();
 
-    await conn.reply(m.chat, '*🚀 Reiniciando bot...*', m);
+    await conn.reply(m.chat, '*🚀 Reiniciando bot...*', m, rcanalr);
 
     await new Promise(resolve => setTimeout(resolve, 3000));
 
     process.exit(0);
   } catch (error) {
     console.error('[ERROR][REINICIO]', error);
-    await conn.reply(m.chat, `❌ Error\n${error.message || error}`, m);
+    await conn.reply(m.chat, `❌ Error\n${error.message || error}`, m, rcanalden2);
   }
 };
 
